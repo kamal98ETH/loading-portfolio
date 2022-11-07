30c121fe0b0c535da28e8a66d13a504148913416
kamal98ETH <bestkacm@gmail.com>
2022-11-07 17:45:58 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from "./images/windows-xp-logo.png";
+import sound from "./sound/windows-xp-startup-sound.mp3";
+
+function start() {
+  let audio = document.getElementById("starting-sound");
+  let starting = document.getElementById("starting-up");
+  let welcome = document.getElementById("welcome");
+  audio.play();
+  console.log(audio)
+  console.log(starting)
+  console.log(welcome)
+  starting.style.display = "none";
+  welcome.style.display = "block";
+  setTimeout(() => {
+    window.location.replace("https://kamal98eth.github.io/portfolio")
+  }, 4000)
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container" onClick={start}>
+      <div className="orange-border up"></div>
+      <div id="light-blue">
+        <div id="starting-up">
+          <img src={logo} alt="windows xp logo" />
+          <h2>Portfolio is starting up...</h2>
+          <p>Click anywhere to start</p>
+        </div>
+        <h1 id="welcome">Welcome</h1>
+      </div>
+      <div className="orange-border down"></div>
+      <audio src={sound} id="starting-sound"></audio>
     </div>
   );
 }
+
+
 
 export default App;
